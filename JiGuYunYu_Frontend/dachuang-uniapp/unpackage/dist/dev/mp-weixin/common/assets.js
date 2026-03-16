a8fa7b9db52333517bc90c089fa2631ec40d2c70
starprince1234 <2320809136@qq.com>
2026-03-16 18:05:25 +0800

    chore(JPY): remove wechat mini-program frontend

--- JiGuYunYu_Frontend/dachuang-uniapp/unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _imports_0$2 = "/static/images/empty.png";
-const _imports_0$1 = "/static/images/logo.png";
-const _imports_0 = "/static/images/ai-avatar.png";
-exports._imports_0 = _imports_0$2;
-exports._imports_0$1 = _imports_0$1;
-exports._imports_0$2 = _imports_0;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
